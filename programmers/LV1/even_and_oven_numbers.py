6e6ec507f76afea625346a41b42e4956225318d5
Lee <lcw9206@gmail.com>
2018-04-19 18:39:37 +0900

    LV1. 짝수와 홀수

---
+++ programmers/LV1/even_and_oven_numbers.py
@@ -0,0 +1,31 @@
+'''
+
+LV1. 짝수와 홀수
+
+evenOrOdd 메소드는 int형 num을 매개변수로 받습니다.
+
+num이 짝수일 경우 Even을 반환하고 홀수인 경우 Odd를 반환하도록 evenOrOdd에 코드를 작성해 보세요.
+
+num은 0이상의 정수이며, num이 음수인 경우는 없습니다.
+
+'''
+
+
+# 내 풀이
+def evenOrOdd(num):
+    return 'Even' if num % 2 == 0 else 'Odd'
+    #함수를 완성하세요
+
+
+#아래는 테스트로 출력해 보기 위한 코드입니다.
+print("결과 : " + evenOrOdd(3))
+print("결과 : " + evenOrOdd(2))
+
+
+# 다른 풀이 : 파이썬은 소괄호가 없으면 논리연산자를 순차적으로 처리한다.
+def evenOrOdd(num):
+    return num % 2 and "Odd" or "Even"
+
+#아래는 테스트로 출력해 보기 위한 코드입니다.
+print("결과 : " + evenOrOdd(3))
+print("결과 : " + evenOrOdd(2))
