041f3b9574e8520b7241720b36e94d78fd6a9764
Lee <lcw9206@gmail.com>
2018-06-21 17:31:56 +0900

    Add LinkedList

---
+++ data-structure/LinkedList.py
@@ -0,0 +1,59 @@
+"""
+
+Linked List
+
+"""
+import unittest
+
+
+# Node Class
+class Node:
+    def __init__(self, item):
+        self.data = item
+        self.next = None
+
+
+# LinkedList Class
+class LinkedList:
+    def __init__(self, item):
+        self.head = Node(item)
+
+    def insert(self, item):
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = Node(item)
+
+    def delete(self, item):
+        if self.head.data == item:  # 첫 번째 노드를 삭제할 경우 수행되는 조건문
+            self.head = self.head.next
+        else:
+            current = self.head
+            while current.next is not None:
+                if current.next.data == item:
+                    current.next = current.next.next    # 다음 노드의 값이 삭제하려는 값일 경우, 현재 노드의 포인터를 다다음 노드로 넘김
+                    return
+                else:
+                    current = current.next
+
+    def print_list(self):
+        current = self.head
+        while current.next is not None:
+            print('{} ->'.format(current.data), end='')
+            current = current.next
+        print(current.data)
+
+
+# Test Class
+class LinkedListTest(unittest.TestCase):
+    def test(self):
+        ll = LinkedList(3)
+        ll.insert(4)
+        ll.insert(5)
+        ll.print_list()
+        ll.delete(4)
+        ll.print_list()
+        ll.insert(7)
+        ll.print_list()
+        ll.delete(5)
+        ll.print_list()
